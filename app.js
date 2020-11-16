6e51b9d122e62898dc157f9aace62859b6627af6
darrendahl <darrencdahl@gmail.com>
2020-11-16 17:58:16 -0300

    simple api websockets

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const express = require("express");
+
+const express = require('express');
+const cors = require('cors')
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server: server, path: '/broadcast'});
+
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+let sessions = []
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(
+  cors({
+    origin: 'http://localhost:5000',
+    optionsSuccessStatus: 200,
+  }),
+);
+
+wss.on("connection", function(ws){
+  ws.on('message', function incoming(data) {
+    if(typeof data === 'string'){
+      ws.send(data);
+    } else {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    }
+  });
+});
+
+//start our server
+server.listen(port, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
